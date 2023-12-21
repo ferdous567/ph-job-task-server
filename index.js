cfea43dac4e4d4bb18ce7a3754525a0950329aaa
ferdous <papiaferdous43@gmail.com>
2023-12-22 00:50:44 +0600

    create task get and post operation

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -10,8 +11,8 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<username>:<password>@cluster0.iknar0j.mongodb.net/?retryWrites=true&w=majority";
+
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.iknar0j.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,10 +28,35 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const taskCollection = client.db('taskDB').collection('allTask');
+
+    // task operation
+    app.post('/tasks', async(req, res) =>{
+        const taskItem = req.body;
+        const result = await taskCollection.insertOne(taskItem);
+        res.send(result);
+    })
+
+    // app.get('/tasks/:email', async(req, res) =>{
+    //     const email = req.query.email;
+    //     console.log(email);
+    //     const query = {email: email}
+    //     const result = await taskCollection.find(query).toArray();
+    //     res.send(result)
+    // })
+
+    app.get('/tasks', async(req, res) =>{
+        console.log(req.query.email);
+        let query = {};
+        if(req.query?.email){
+            query = {email: req.query.email}
+        }
+        const result = await taskCollection.find(query).toArray();
+        res.send(result);
+    })
 
 
 
-    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
